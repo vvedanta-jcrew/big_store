a2b86ec06a9b45a13e16e38cbceb658085dcf78a
Dilip <43420663+vvedanta-jcrew@users.noreply.github.com>
2019-10-20 00:06:47 -0400

    First cut commit

---
+++ bigstore/product/api.py
@@ -0,0 +1,18 @@
+# Product Service
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+	def get(self):
+	    return {
+	    'products': ['Jeans', 'Shirts', 'Sweat Shirts', 'Sweaters']
+	    }
+
+api.add_resource(Product, '/')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80, debug=True)		
---
+++ bigstore/website/index.php
@@ -0,0 +1,46 @@
+<html>
+	<head>
+		<title>BIG STORE</title>
+	</head>
+
+	<body>
+		<h1>Welcome to the B.I.G store!</h1>
+		<h1>Mens:</h1>
+		<ul>
+			<?php
+				$json = file_get_contents('http://product-service');
+				$obj = json_decode($json);
+
+				$products = $obj->products;
+				foreach ($products as $product) {
+					echo "<li>$product</li>";
+				}
+			?>
+		</ul>
+	</body>
+		<h1>Womens:</h1>
+		<ul>
+			<?php
+				$json = file_get_contents('http://product-service');
+				$obj = json_decode($json);
+
+				$products = $obj->products;
+				foreach ($products as $product) {
+					echo "<li>$product</li>";
+				}
+			?>
+		</ul>
+	</body>
+		<h1>Kids:</h1>
+		<ul>
+			<?php
+				$json = file_get_contents('http://product-service');
+				$obj = json_decode($json);
+
+				$products = $obj->products;
+				foreach ($products as $product) {
+					echo "<li>$product</li>";
+				}
+			?>
+		</ul>
+<html>
